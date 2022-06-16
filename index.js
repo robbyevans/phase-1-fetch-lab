b25a49a95c0817f89b00c23a8af0d03f62c2a433
Evans Rutto <robbyevans001@gmail.com>
2022-06-16 09:19:46 +0300

    commit tree c660aa28e61b29cb8a632bcd2e8fc016bf3635c3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp)=>resp.json())
+  .then((data)=>renderBooks(data))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -10,8 +14,10 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+fetchBooks()
